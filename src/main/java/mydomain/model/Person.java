2adf70dec59ea2c4c38ea50fc1b46e1543685e2d
Jared Wiltshire <jazdw@users.noreply.github.com>
2015-11-27 13:03:22 +1100

    Merge failure test case

--- src/main/java/mydomain/model/Person.java
+++ src/main/java/mydomain/model/Person.java
@@ -7,11 +7,15 @@ public class Person
 {
     @Id
     Long id;
+    
+    @Version
+    long version;
 
     String name;
 
-    public Person(long id, String name)
+    public Person(long version, long id, String name)
     {
+        this.version = version;
         this.id = id;
         this.name = name;
     }
@@ -19,6 +23,19 @@ public class Person
     public String getName()
     {
         return name;
+    }
+    
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+    
+    public long getVersion() {
+        return version;
+    }
+    
+    public void setVersion(long version) {
+        this.version = version;
     }
 
     public Long getId()
--- src/test/java/org/datanucleus/test/SimpleTest.java
+++ src/test/java/org/datanucleus/test/SimpleTest.java
@@ -16,13 +16,41 @@ public class SimpleTest
         NucleusLogger.GENERAL.info(">> test START");
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("MyTest");
 
+        Person jim = new Person(0, 1, "Jim");
+        
         EntityManager em = emf.createEntityManager();
         EntityTransaction tx = em.getTransaction();
         try
         {
             tx.begin();
-
-            // [INSERT code here to persist object required for testing]
+            em.persist(jim);
+            tx.commit();
+        }
+        catch (Throwable thr)
+        {
+            NucleusLogger.GENERAL.error(">> Exception in test", thr);
+            fail("Failed test : " + thr.getMessage());
+        }
+        finally 
+        {
+            if (tx.isActive())
+            {
+                tx.rollback();
+            }
+            em.close();
+        }
+        
+        // e.g. de-serialised
+        Person jimBrown = new Person(1, 1, "Jim Brown");
+        
+        em = emf.createEntityManager();
+        tx = em.getTransaction();
+        try
+        {
+            tx.begin();
+            
+            em.merge(jimBrown);
+            
             tx.commit();
         }
         catch (Throwable thr)
